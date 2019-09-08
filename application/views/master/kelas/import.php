3dcb461c5061076353f161a7b9bfa46e9fb3f752
Web Code Sharing <webcodesharing@gmail.com>
2019-09-09 06:02:45 +0700

    Menambahkan Fitur Import Excel Kelas

---
+++ application/views/master/kelas/import.php
@@ -0,0 +1,122 @@
+<div class="box">
+    <div class="box-header with-border">
+        <h3 class="box-title"><?= $subjudul ?></h3>
+        <div class="box-tools pull-right">
+            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+            </button>
+        </div>
+    </div>
+    <div class="box-body">
+        <ul class="alert alert-info" style="padding-left: 40px">
+            <li>Silahkan import data dari excel, menggunakan format yang sudah disediakan</li>
+            <li>Data tidak boleh ada yang kosong, harus terisi semua.</li>
+            <li>Untuk data jurusan, hanya bisa diisi menggunakan ID Jurusan. <a data-toggle="modal" href="#jurusanId" style="text-decoration:none" class="btn btn-xs btn-primary">Lihat ID</a>.</li>
+        </ul>
+        <div class="text-center">
+            <a href="<?= base_url('uploads/import/format/kelas.xlsx') ?>" class="btn-default btn">Download Format</a>
+        </div>
+        <br>
+        <div class="row">
+            <?= form_open_multipart('kelas/preview'); ?>
+            <label for="file" class="col-sm-offset-1 col-sm-3 text-right">Pilih File</label>
+            <div class="col-sm-4">
+                <div class="form-group">
+                    <input type="file" name="upload_file">
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <button name="preview" type="submit" class="btn btn-sm btn-success">Preview</button>
+            </div>
+            <?= form_close(); ?>
+            <div class="col-sm-6 col-sm-offset-3">
+                <?php if (isset($_POST['preview'])) : ?>
+                    <br>
+                    <h4>Preview Data</h4>
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <td>No</td>
+                                <td>Kelas</td>
+                                <td>Jurusan</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                                $status = true;
+                                if (empty($import)) {
+                                    echo '<tr><td colspan="2" class="text-center">Data kosong! pastikan anda menggunakan format yang telah disediakan.</td></tr>';
+                                } else {
+                                    $no = 1;
+                                    foreach ($import as $data) :
+                                        ?>
+                                    <tr>
+                                        <td><?= $no++; ?></td>
+                                        <td class="<?= $data['kelas'] == null ? 'bg-danger' : ''; ?>">
+                                            <?= $data['kelas'] == null ? 'BELUM DIISI' : $data['kelas']; ?>
+                                        </td>
+                                        <td class="<?= $data['jurusan'] == null ? 'bg-danger' : ''; ?>">
+                                            <?= $data['jurusan'] == null ? 'BELUM DIISI' : $data['jurusan'];; ?>
+                                        </td>
+                                    </tr>
+                            <?php
+                                        if ($data['kelas'] == null || $data['jurusan'] == null) {
+                                            $status = false;
+                                        }
+                                    endforeach;
+                                }
+                                ?>
+                        </tbody>
+                    </table>
+                    <?php if ($status) : ?>
+
+                        <?= form_open('kelas/do_import', null, ['data' => json_encode($import)]); ?>
+                        <button type='submit' class='btn btn-block btn-flat bg-purple'>Import</button>
+                        <?= form_close(); ?>
+
+                    <?php endif; ?>
+                    <br>
+                <?php endif; ?>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="jurusanId">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span></button>
+                <h4 class="modal-title">Data Jurusan</h4>
+            </div>
+            <div class="modal-body">
+                <table id="jurusan" class="table table-condensed table-striped">
+                    <thead>
+                        <th>ID</th>
+                        <th>Jurusan</th>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($jurusan as $j) : ?>
+                            <tr>
+                                <td><?= $j->id_jurusan; ?></td>
+                                <td><?= $j->nama_jurusan; ?></td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+    $(document).ready(function() {
+        let table;
+        table = $("#jurusan").DataTable({
+            "lengthMenu": [
+                [5, 10, 25, 50, 100, -1],
+                [5, 10, 25, 50, 100, "All"]
+            ],
+        });
+    });
+</script>
