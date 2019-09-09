7a35b9e2aaed06644ff047781987d3dd9bf4a4c0
Web Code Sharing <webcodesharing@gmail.com>
2019-09-09 09:58:40 +0700

    fix footer version

--- application/views/_templates/dashboard/_footer.php
+++ application/views/_templates/dashboard/_footer.php
@@ -1,75 +1,75 @@
 			</section>
 			<!-- /.content -->
-		</div>
-		<!-- /.content-wrapper -->
-
-		<!-- Main Footer -->
-		<footer class="main-footer">
-			<!-- To the right -->
-			<div class="pull-right hidden-xs">
-				Computer Based Test v.1.0
 			</div>
-			<!-- Default to the left -->
-			<strong>Copyright &copy; 2019. By <a href="https://instagram.com/mghifariarfan">Muhammad Ghifari Arfananda</a>. </strong> All rights reserved
-		</footer>
+			<!-- /.content-wrapper -->
 
-	</div>
+			<!-- Main Footer -->
+			<footer class="main-footer">
+				<!-- To the right -->
+				<div class="pull-right hidden-xs">
+					Computer Based Test v2
+				</div>
+				<!-- Default to the left -->
+				<strong>Copyright &copy; 2019. By <a href="https://instagram.com/mghifariarfan">Muhammad Ghifari Arfananda</a>. </strong> All rights reserved
+			</footer>
 
-	<!-- Required JS -->
-	<script src="<?=base_url()?>assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-	<script src="<?=base_url()?>assets/bower_components/datatables.net-bs/js/jquery.dataTables.min.js"></script>
-	<script src="<?=base_url()?>assets/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
+			</div>
 
-	<!-- Datatables Buttons -->
-	<script src="<?=base_url()?>assets/bower_components/datatables.net-bs/plugins/Buttons-1.5.6/js/dataTables.buttons.min.js"></script>
-	<script src="<?=base_url()?>assets/bower_components/datatables.net-bs/plugins/Buttons-1.5.6/js/buttons.bootstrap.min.js"></script>
-	<script src="<?=base_url()?>assets/bower_components/datatables.net-bs/plugins/JSZip-2.5.0/jszip.min.js"></script>
-	<script src="<?=base_url()?>assets/bower_components/datatables.net-bs/plugins/pdfmake-0.1.36/pdfmake.min.js"></script>
-	<script src="<?=base_url()?>assets/bower_components/datatables.net-bs/plugins/pdfmake-0.1.36/vfs_fonts.js"></script>
-	<script src="<?=base_url()?>assets/bower_components/datatables.net-bs/plugins/Buttons-1.5.6/js/buttons.html5.min.js"></script>
-	<script src="<?=base_url()?>assets/bower_components/datatables.net-bs/plugins/Buttons-1.5.6/js/buttons.print.min.js"></script>
-	<script src="<?=base_url()?>assets/bower_components/datatables.net-bs/plugins/Buttons-1.5.6/js/buttons.colVis.min.js"></script>
+			<!-- Required JS -->
+			<script src="<?= base_url() ?>assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+			<script src="<?= base_url() ?>assets/bower_components/datatables.net-bs/js/jquery.dataTables.min.js"></script>
+			<script src="<?= base_url() ?>assets/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
 
-	<script src="<?=base_url()?>assets/bower_components/pace/pace.min.js"></script>
-	<script src="<?=base_url()?>assets/dist/js/adminlte.min.js"></script>
+			<!-- Datatables Buttons -->
+			<script src="<?= base_url() ?>assets/bower_components/datatables.net-bs/plugins/Buttons-1.5.6/js/dataTables.buttons.min.js"></script>
+			<script src="<?= base_url() ?>assets/bower_components/datatables.net-bs/plugins/Buttons-1.5.6/js/buttons.bootstrap.min.js"></script>
+			<script src="<?= base_url() ?>assets/bower_components/datatables.net-bs/plugins/JSZip-2.5.0/jszip.min.js"></script>
+			<script src="<?= base_url() ?>assets/bower_components/datatables.net-bs/plugins/pdfmake-0.1.36/pdfmake.min.js"></script>
+			<script src="<?= base_url() ?>assets/bower_components/datatables.net-bs/plugins/pdfmake-0.1.36/vfs_fonts.js"></script>
+			<script src="<?= base_url() ?>assets/bower_components/datatables.net-bs/plugins/Buttons-1.5.6/js/buttons.html5.min.js"></script>
+			<script src="<?= base_url() ?>assets/bower_components/datatables.net-bs/plugins/Buttons-1.5.6/js/buttons.print.min.js"></script>
+			<script src="<?= base_url() ?>assets/bower_components/datatables.net-bs/plugins/Buttons-1.5.6/js/buttons.colVis.min.js"></script>
 
-	<!-- Textarea editor -->
-	<script src="<?=base_url()?>assets/bower_components/codemirror/lib/codemirror.min.js"></script>
-	<script src="<?=base_url()?>assets/bower_components/codemirror/mode/xml.min.js"></script>
-	<script src="<?=base_url()?>assets/bower_components/froala_editor/js/froala_editor.pkgd.min.js"></script>
+			<script src="<?= base_url() ?>assets/bower_components/pace/pace.min.js"></script>
+			<script src="<?= base_url() ?>assets/dist/js/adminlte.min.js"></script>
 
-	<!-- App JS -->
-	<script src="<?=base_url()?>assets/dist/js/app/dashboard.js"></script>
+			<!-- Textarea editor -->
+			<script src="<?= base_url() ?>assets/bower_components/codemirror/lib/codemirror.min.js"></script>
+			<script src="<?= base_url() ?>assets/bower_components/codemirror/mode/xml.min.js"></script>
+			<script src="<?= base_url() ?>assets/bower_components/froala_editor/js/froala_editor.pkgd.min.js"></script>
 
-	<!-- Custom JS -->
-	<script type="text/javascript">
-		$.fn.dataTableExt.oApi.fnPagingInfo = function (oSettings) {
-			return {
-				"iStart": oSettings._iDisplayStart,
-				"iEnd": oSettings.fnDisplayEnd(),
-				"iLength": oSettings._iDisplayLength,
-				"iTotal": oSettings.fnRecordsTotal(),
-				"iFilteredTotal": oSettings.fnRecordsDisplay(),
-				"iPage": Math.ceil(oSettings._iDisplayStart / oSettings._iDisplayLength),
-				"iTotalPages": Math.ceil(oSettings.fnRecordsDisplay() / oSettings._iDisplayLength)
-			};
-		};
+			<!-- App JS -->
+			<script src="<?= base_url() ?>assets/dist/js/app/dashboard.js"></script>
 
-		function ajaxcsrf(){
-			var csrfname = '<?=$this->security->get_csrf_token_name()?>';
-			var csrfhash = '<?=$this->security->get_csrf_hash()?>';
-			var csrf = {};
-			csrf[csrfname] = csrfhash;
-			$.ajaxSetup({
-				"data": csrf
-			});
-		}
+			<!-- Custom JS -->
+			<script type="text/javascript">
+				$.fn.dataTableExt.oApi.fnPagingInfo = function(oSettings) {
+					return {
+						"iStart": oSettings._iDisplayStart,
+						"iEnd": oSettings.fnDisplayEnd(),
+						"iLength": oSettings._iDisplayLength,
+						"iTotal": oSettings.fnRecordsTotal(),
+						"iFilteredTotal": oSettings.fnRecordsDisplay(),
+						"iPage": Math.ceil(oSettings._iDisplayStart / oSettings._iDisplayLength),
+						"iTotalPages": Math.ceil(oSettings.fnRecordsDisplay() / oSettings._iDisplayLength)
+					};
+				};
 
-		function reload_ajax(){                
-			table.ajax.reload(null, false);
-		}
-	</script>
+				function ajaxcsrf() {
+					var csrfname = '<?= $this->security->get_csrf_token_name() ?>';
+					var csrfhash = '<?= $this->security->get_csrf_hash() ?>';
+					var csrf = {};
+					csrf[csrfname] = csrfhash;
+					$.ajaxSetup({
+						"data": csrf
+					});
+				}
 
-</body>
+				function reload_ajax() {
+					table.ajax.reload(null, false);
+				}
+			</script>
 
-</html>
+			</body>
+
+			</html>
--- application/views/_templates/topnav/_footer.php
+++ application/views/_templates/topnav/_footer.php
@@ -6,9 +6,9 @@
 <!-- /.content-wrapper -->
 <footer class="main-footer">
 	<div class="container">
-		<?=strftime('%A, %d %B %Y')?>, <span class="live-clock"><?=date('H:i:s')?></span>
+		<?= strftime('%A, %d %B %Y') ?>, <span class="live-clock"><?= date('H:i:s') ?></span>
 		<div class="pull-right hidden-xs">
-			<b>Ujian Online</b> v1.0
+			<b>Ujian Online</b> v2
 		</div>
 	</div>
 	<!-- /.container -->
@@ -16,22 +16,21 @@
 </div>
 <!-- ./wrapper -->
 
-<script src="<?=base_url()?>assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-<script src="<?=base_url()?>assets/dist/js/adminlte.min.js"></script>
-<script src="<?=base_url()?>assets/bower_components/pace/pace.min.js"></script>
+<script src="<?= base_url() ?>assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="<?= base_url() ?>assets/dist/js/adminlte.min.js"></script>
+<script src="<?= base_url() ?>assets/bower_components/pace/pace.min.js"></script>
 
 <script type="text/javascript">
-	
-	function sisawaktu(t){
+	function sisawaktu(t) {
 		var time = new Date(t);
 		var n = new Date();
-		var x = setInterval(function(){
+		var x = setInterval(function() {
 			var now = new Date().getTime();
-			var dis = time.getTime() - now;            
-            var h = Math.floor((dis % (1000 * 60 * 60 * 60)) / (1000 * 60 * 60));
+			var dis = time.getTime() - now;
+			var h = Math.floor((dis % (1000 * 60 * 60 * 60)) / (1000 * 60 * 60));
 			var m = Math.floor((dis % (1000 * 60 * 60)) / (1000 * 60));
 			var s = Math.floor((dis % (1000 * 60)) / (1000));
-		    h = ("0" + h).slice(-2);
+			h = ("0" + h).slice(-2);
 			m = ("0" + m).slice(-2);
 			s = ("0" + s).slice(-2);
 			var cd = h + ":" + m + ":" + s;
@@ -42,10 +41,10 @@
 		}, (time.getTime() - n.getTime()));
 	}
 
-	function countdown(t){
+	function countdown(t) {
 		var time = new Date(t);
 		var n = new Date();
-		var x = setInterval(function(){
+		var x = setInterval(function() {
 			var now = new Date().getTime();
 			var dis = time.getTime() - now;
 			var d = Math.floor(dis / (1000 * 60 * 60 * 24));
@@ -58,32 +57,34 @@
 			s = ("0" + s).slice(-2);
 			var cd = d + " Hari, " + h + " Jam, " + m + " Menit, " + s + " Detik ";
 			$('.countdown').html(cd);
-			
+
 			setTimeout(function() {
 				location.reload()
 			}, dis);
 		}, 1000);
 	}
 
-	function ajaxcsrf(){
-		var csrfname = '<?=$this->security->get_csrf_token_name()?>';
-		var csrfhash = '<?=$this->security->get_csrf_hash()?>';
+	function ajaxcsrf() {
+		var csrfname = '<?= $this->security->get_csrf_token_name() ?>';
+		var csrfhash = '<?= $this->security->get_csrf_hash() ?>';
 		var csrf = {};
 		csrf[csrfname] = csrfhash;
 		$.ajaxSetup({
 			"data": csrf
 		});
 	}
-	
-	$(document).ready(function(){
+
+	$(document).ready(function() {
 		setInterval(function() {
 			var date = new Date();
-			var h = date.getHours(), m = date.getMinutes(), s = date.getSeconds();
+			var h = date.getHours(),
+				m = date.getMinutes(),
+				s = date.getSeconds();
 			h = ("0" + h).slice(-2);
 			m = ("0" + m).slice(-2);
 			s = ("0" + s).slice(-2);
 
-			var time = h + ":" + m + ":" + s ;
+			var time = h + ":" + m + ":" + s;
 			$('.live-clock').html(time);
 		}, 1000);
 	});
