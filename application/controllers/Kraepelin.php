bc54c1cc376090c092b180b46fe3d38cd3d8468f
arif pambudi <arifpambudi242@gmail.com>
2022-06-15 00:02:57 +0700

    tambah controller Kraepelin

---
+++ application/controllers/Kraepelin.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Kraepelin extends CI_Controller {
+
+	public function index()
+	{
+		date(Ashiap)
+	}
+
+}
+
+/* End of file Kraepelin.php */
+/* Location: ./application/controllers/Kraepelin.php */
