c72ed7221281b6bce1901c9fa74be14246828067
aries <ariesilham234@gmial.com>
2022-06-14 19:02:02 +0700

    gx jadi

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -1,11 +1,13 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Dashboard extends CI_Controller {
+class Dashboard extends CI_Controller
+{
 
-	public function __construct(){
+	public function __construct()
+	{
 		parent::__construct();
-		if (!$this->ion_auth->logged_in()){
+		if (!$this->ion_auth->logged_in()) {
 			redirect('auth');
 		}
 		$this->load->model('Dashboard_model', 'dashboard');
@@ -53,15 +55,15 @@ class Dashboard extends CI_Controller {
 			'subjudul'	=> 'Data Aplikasi',
 		];
 
-		if ( $this->ion_auth->is_admin() ) {
+		if ($this->ion_auth->is_admin()) {
 			$data['info_box'] = $this->admin_box();
-		} elseif ( $this->ion_auth->in_group('dosen') ) {
+		} elseif ($this->ion_auth->in_group('dosen')) {
 			$matkul = ['matkul' => 'dosen.matkul_id=matkul.id_matkul'];
 			$data['dosen'] = $this->dashboard->get_where('dosen', 'nip', $user->username, $matkul)->row();
 
 			$kelas = ['kelas' => 'kelas_dosen.kelas_id=kelas.id_kelas'];
-			$data['kelas'] = $this->dashboard->get_where('kelas_dosen', 'dosen_id' , $data['dosen']->id_dosen, $kelas, ['nama_kelas'=>'ASC'])->result();
-		}else{
+			$data['kelas'] = $this->dashboard->get_where('kelas_dosen', 'dosen_id', $data['dosen']->id_dosen, $kelas, ['nama_kelas' => 'ASC'])->result();
+		} else {
 			$join = [
 				'kelas b' 	=> 'a.kelas_id = b.id_kelas',
 				'jurusan c'	=> 'b.jurusan_id = c.id_jurusan'
